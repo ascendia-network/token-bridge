0dd28ea30830b6ed3d26609ec4bdd81f1367a6f6
svin <svinerus@gmail.com>
2025-02-06 14:56:52 +0200

    update contracts, write some tests [wip]

--- bridge-svm/programs/bridge-svm/src/lib.rs
+++ bridge-svm/programs/bridge-svm/src/lib.rs
@@ -1,76 +1,13 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Token, TokenAccount, Transfer};
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token::{self, Token, Transfer};
+use anchor_spl::token_interface::{ Mint, TokenAccount, TokenInterface, };
 
-declare_id!("GrDsFgHMrguqLqhb9JGvE26wTCbwPiLPhFGMsxeDt5Xd");
-
-#[derive(Accounts)]
-pub struct Lock<'info> {
-    #[account(mut)]
-    pub authority: Signer<'info>,
-
-    #[account(mut)]
-    pub from: Account<'info, TokenAccount>,
-
-    #[account(mut)]
-    pub to: Account<'info, TokenAccount>,
-
-    #[account(
-        mut,
-        seeds = [b"global_state"],
-        bump,
-        constraint = state.is_enable
-    )]
-    pub state: Account<'info, GlobalState>,
-
-    pub token_program: Program<'info, Token>,
-}
-
-#[derive(Accounts)]
-pub struct Unlock<'info> {
-    #[account(mut)]
-    pub authority: Signer<'info>,
-
-    #[account(mut)]
-    pub user: Signer<'info>,
-
-    #[account(mut)]
-    pub from: Account<'info, TokenAccount>,
-
-    #[account(mut)]
-    pub to: Account<'info, TokenAccount>,
-
-    #[account(
-        init_if_needed,
-        payer = user,
-        space = 16,
-        seeds = [user.key().as_ref()],
-        bump,
-    )]
-    pub nonce_account: Account<'info, NonceAccount>,
-
-    pub token_program: Program<'info, Token>,
-
-    #[account(
-        mut,
-        seeds = [b"global_state"],
-        bump,
-        owner = crate::ID,
-        constraint = state.is_enable
-    )]
-    pub state: Account<'info, GlobalState>,
-
-    pub system_program: Program<'info, System>,
-}
+declare_id!("6TcA8kPmipG2xQtwYWQQq4CioTMbxHLjSCnpu1jqA6pZ");
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(
-        init,
-        seeds = [b"global_state"],
-        bump,
-        payer = admin,
-        space = 992
-    )]
+    #[account(        init,        seeds = [b"global_state"],        bump,        payer = admin,        space = 992    )]
     pub state: Account<'info, GlobalState>,
 
     #[account(mut)]
@@ -78,6 +15,151 @@ pub struct Initialize<'info> {
 
     pub system_program: Program<'info, System>,
 }
+
+
+
+
+
+
+
+#[derive(Accounts)]
+pub struct CreateTokenAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        space = TokenConfig::ACCOUNT_SIZE,
+        payer = signer,
+        seeds = [TokenConfig::SEED_PREFIX.as_bytes(), mint.key().as_ref()],
+        bump
+    )]
+    pub bridge_token: Account<'info, TokenConfig>,
+
+    #[account(
+        init,
+        payer = signer,
+        token::mint = mint,
+        token::authority = bridge_token,
+    )]
+    pub bridge_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct TokenConfig {
+    pub token: Pubkey,      // Public key of the token
+    pub amb_token: [u8; 20],  //
+    pub bump: u8,           // Nonce for the program-derived address
+}
+
+impl TokenConfig {
+    pub const SEED_PREFIX: &'static str = "token";
+    pub const ACCOUNT_SIZE: usize = 8 + 32 + 20 + 1;     // discriminator (8)  + Pubkey (32) + address (20)     + Bump (1) _
+    pub fn new(token: Pubkey, amb_token: [u8; 20], bump: u8) -> Self {
+        Self {            token,            amb_token,            bump,        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#[derive(Accounts)]
+pub struct Lock<'info> {
+    #[account(        mut,        seeds = [b"global_state"],        bump,        constraint = state.is_enable    )]
+    pub state: Account<'info, GlobalState>,
+
+    #[account(mut)]
+    pub sender: Signer<'info>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = sender,
+    )]
+    pub sender_token_account: InterfaceAccount<'info, TokenAccount>,
+
+
+    #[account(
+        mut,
+        seeds = [TokenConfig::SEED_PREFIX.as_bytes(), mint.key().as_ref()],
+        bump = bridge_token.bump
+    )]
+    pub bridge_token: Account<'info, TokenConfig>,
+
+    #[account(
+        token::mint = mint,
+        token::authority = bridge_token,
+    )]
+    pub bridge_token_account: InterfaceAccount<'info, TokenAccount>,
+
+
+
+
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+
+
+
+
+
+
+//
+// #[derive(Accounts)]
+// pub struct Unlock<'info> {
+//     #[account(mut)]
+//     pub authority: Signer<'info>,
+//
+//     #[account(mut)]
+//     pub user: Signer<'info>,
+//
+//     #[account(mut)]
+//     pub to: Account<'info, TokenAccount>,
+//
+//     #[account(
+//         init_if_needed,
+//         payer = user,
+//         space = 16,
+//         seeds = [user.key().as_ref()],
+//         bump,
+//     )]
+//     pub nonce_account: Account<'info, NonceAccount>,
+//
+//     pub token_program: Program<'info, Token>,
+//
+//     #[account(
+//         mut,
+//         seeds = [b"global_state"],
+//         bump,
+//         owner = crate::ID,
+//         constraint = state.is_enable
+//     )]
+//     pub state: Account<'info, GlobalState>,
+//
+//     pub system_program: Program<'info, System>,
+// }
 
 #[derive(Accounts)]
 pub struct UpdateState<'info> {
@@ -99,7 +181,7 @@ pub struct GlobalState {
     pub admin: Pubkey,
     pub nonce: u64,
     pub is_enable: bool,
-    pub tokens: Vec<(Pubkey, String)>,
+    // pub tokens: Vec<(Pubkey, String)>,
 }
 
 #[error_code]
@@ -124,26 +206,32 @@ pub mod multisig_nonce {
         state.admin = Pubkey::from_str(ADMIN).unwrap_or(ctx.accounts.admin.key());
         state.nonce = 0;
         state.is_enable = true;
-        state.tokens = Vec::new();
+        // state.tokens = Vec::new();
         Ok(())
     }
 
-    pub fn add_token(ctx: Context<UpdateState>, token: (Pubkey, String)) -> Result<()> {
-        if ctx.accounts.state.tokens.len() < 12 {
-            ctx.accounts.state.tokens.push(token.clone());
-            msg!("Add token: {}", token.0);
-        }
+    pub fn initialize_token(ctx: Context<CreateTokenAccount>, amb_token: [u8; 20]) -> Result<()> {
+        // todo use new
+        ctx.accounts.bridge_token.amb_token = amb_token;
         Ok(())
     }
 
-    pub fn delete_token(ctx: Context<UpdateState>, token: Pubkey) -> Result<()> {
-        if let Some(index) = ctx.accounts.state.tokens.iter().position(|x| x.0 == token) {
-            ctx.accounts.state.tokens.remove(index);
-            msg!("Delete token: {}", token);
-        }
-        Ok(())
-    }
+    // pub fn add_token(ctx: Context<UpdateState>, token: (Pubkey, String)) -> Result<()> {
+    //     if ctx.accounts.state.tokens.len() < 12 {
+    //         ctx.accounts.state.tokens.push(token.clone());
+    //         msg!("Add token: {}", token.0);
+    //     }
+    //     Ok(())
+    // }
 
+    // pub fn delete_token(ctx: Context<UpdateState>, token: Pubkey) -> Result<()> {
+    //     if let Some(index) = ctx.accounts.state.tokens.iter().position(|x| x.0 == token) {
+    //         ctx.accounts.state.tokens.remove(index);
+    //         msg!("Delete token: {}", token);
+    //     }
+    //     Ok(())
+    // }
+    //
     pub fn disable(ctx: Context<UpdateState>) -> Result<()> {
         ctx.accounts.state.is_enable = false;
         Ok(())
@@ -156,30 +244,29 @@ pub mod multisig_nonce {
 
     pub fn lock(ctx: Context<Lock>, amount: u64, destination: String) -> Result<()> {
         ctx.accounts.state.nonce += 1;
-        if !ctx
-            .accounts
-            .state
-            .tokens
-            .iter()
-            .any(|(pubkey, _)| pubkey == &ctx.accounts.to.mint)
-        {
-            return Err(ErrorCode::InvalidToken.into());
-        }
+        // if !ctx
+        //     .accounts
+        //     .state
+        //     .tokens
+        //     .iter()
+        //     .any(|(pubkey, _)| pubkey == &ctx.accounts.to.mint)
+        // {
+        //     return Err(ErrorCode::InvalidToken.into());
+        // }
 
         let cpi_accounts = Transfer {
-            from: ctx.accounts.from.to_account_info(),
-            to: ctx.accounts.to.to_account_info(),
-            authority: ctx.accounts.authority.to_account_info(),
+            from: ctx.accounts.sender_token_account.to_account_info(),
+            to: ctx.accounts.bridge_token_account.to_account_info(),
+            authority: ctx.accounts.sender.to_account_info(),
         };
 
         let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
         token::transfer(cpi_ctx, amount)?;
 
         msg!(
-            "Lock, token: {}, from: {}, to: {}, amount: {}, destination: {}, nonce: {}",
+            "Lock, token: {}, from: {}, amount: {}, destination: {}, nonce: {}",
             ctx.accounts.token_program.key(),
-            ctx.accounts.from.key(),
-            ctx.accounts.to.key(),
+            ctx.accounts.sender.key(),
             amount,
             destination,
             ctx.accounts.state.nonce
@@ -187,36 +274,36 @@ pub mod multisig_nonce {
         Ok(())
     }
 
-    pub fn unlock(ctx: Context<Unlock>, amount: u64, nonce_value: u64) -> Result<()> {
-        let nonce = &mut ctx.accounts.nonce_account;
-        if nonce_value != nonce.nonce_counter {
-            return Err(ErrorCode::InvalidNonce.into());
-        }
-
-        let accounts = ctx.remaining_accounts;
-        for account in accounts {
-            if !account.is_signer {
-                return Err(ErrorCode::MissingSignature.into());
-            }
-        }
-
-        let cpi_accounts = Transfer {
-            from: ctx.accounts.from.to_account_info(),
-            to: ctx.accounts.to.to_account_info(),
-            authority: ctx.accounts.authority.to_account_info(),
-        };
-
-        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
-        token::transfer(cpi_ctx, amount)?;
-
-        nonce.nonce_counter += 1;
-        msg!(
-            "Unlock, token: {}, from: {}, to: {}, amount: {}",
-            ctx.accounts.token_program.key(),
-            ctx.accounts.from.key(),
-            ctx.accounts.to.key(),
-            amount
-        );
-        Ok(())
-    }
+    // pub fn unlock(ctx: Context<Unlock>, amount: u64, nonce_value: u64) -> Result<()> {
+    //     let nonce = &mut ctx.accounts.nonce_account;
+    //     if nonce_value != nonce.nonce_counter {
+    //         return Err(ErrorCode::InvalidNonce.into());
+    //     }
+    //
+    //     let accounts = ctx.remaining_accounts;
+    //     for account in accounts {
+    //         if !account.is_signer {
+    //             return Err(ErrorCode::MissingSignature.into());
+    //         }
+    //     }
+    //
+    //     let cpi_accounts = Transfer {
+    //         from: ctx.accounts.from.to_account_info(),
+    //         to: ctx.accounts.to.to_account_info(),
+    //         authority: ctx.accounts.authority.to_account_info(),
+    //     };
+    //
+    //     let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+    //     token::transfer(cpi_ctx, amount)?;
+    //
+    //     nonce.nonce_counter += 1;
+    //     msg!(
+    //         "Unlock, token: {}, from: {}, to: {}, amount: {}",
+    //         ctx.accounts.token_program.key(),
+    //         ctx.accounts.from.key(),
+    //         ctx.accounts.to.key(),
+    //         amount
+    //     );
+    //     Ok(())
+    // }
 }
---
+++ bridge-svm/tests/bridge-svm.ts
@@ -0,0 +1,262 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { type Connection, Keypair, PublicKey, type Signer, SystemProgram, Transaction } from '@solana/web3.js';
+
+import { MultisigNonce } from "../target/types/multisig_nonce";
+import {
+  createMint,
+  getAssociatedTokenAddressSync,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  TOKEN_PROGRAM_ID
+} from "@solana/spl-token";
+
+
+import assert from "assert";
+
+
+describe("my-project", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.local());
+
+  const program = anchor.workspace.MultisigNonce as Program<MultisigNonce>;
+
+  let connection;
+  let admin, user;
+  let state, vault;
+  let tokenMintA;
+  let userTokenAddressA;
+
+  it("airdropping sol", async () => {
+
+    connection = program.provider.connection;
+
+    admin = anchor.web3.Keypair.generate();
+    user = anchor.web3.Keypair.generate();
+    tokenMintA = Keypair.generate();
+
+    [state] = PublicKey.findProgramAddressSync([Buffer.from("global_state")], program.programId);
+
+    console.log("Admin's wallet address is : ", admin.publicKey.toBase58())
+    console.log("User's wallet address is : ", user.publicKey.toBase58())
+    console.log("tokenMintA", tokenMintA.publicKey)
+
+    await requestSol(admin, connection);
+    await requestSol(user, connection);
+
+
+    userTokenAddressA = await mintingTokens({
+      connection,
+      creator: admin,
+      holder: user,
+      tokenKeypair: tokenMintA,
+    })
+    console.log("userTokenAddressA", userTokenAddressA.address)
+
+  })
+
+
+  it("initializing", async () => {
+
+    const tx = await program.methods.initialize().accounts({
+      state: state,
+      admin: admin.publicKey,
+      systemProgram: SystemProgram.programId
+    }).signers([admin]).rpc();
+    console.log("Your transaction signature", tx);
+
+    const accountState = await program.account.globalState.fetch(state);
+    console.log("accountState", accountState)
+    assert.ok(accountState.admin.equals(admin.publicKey));
+    assert.ok(accountState.isEnable);
+
+
+    const [bridge_token_pda] = PublicKey.findProgramAddressSync([Buffer.from("token"), tokenMintA.publicKey.toBuffer()], program.programId)
+    const bridge_token_account = getAssociatedTokenAddressSync(tokenMintA.publicKey, bridge_token_pda, true);
+
+
+    await program.methods.initializeToken(hexToUint8Array("0x1111472FCa4260505EcE4AcD07717CADa41c1111")).accounts({
+      signer: admin.publicKey,
+      bridge_token: bridge_token_pda,
+      bridge_token_account: bridge_token_account,
+
+      mint: tokenMintA.publicKey,
+
+      tokenProgram: TOKEN_PROGRAM_ID,
+      system_program: SystemProgram.programId,
+    }).signers([admin]).rpc();
+
+    const tokenConfigState = await program.account.tokenConfig.fetch(bridge_token_pda);
+    console.log("tokenConfigState",  tokenConfigState)
+
+    // todo this not working
+    const tokenBalanceB = await connection.getTokenAccountBalance(bridge_token_account);
+    console.log("tokenBalanceB", tokenBalanceB)
+
+  });
+
+  it('lock', async () => {
+    const amount = new anchor.BN(100);
+
+
+    // const [poolPda] = PublicKey.findProgramAddressSync([Buffer.from("bridge?"), tokenKeypair.publicKey.toBuffer()], program.programId)
+    // const vaultTokenAddress = getAssociatedTokenAddressSync(tokenKeypair.publicKey, poolPda, true);
+
+
+
+
+    const userAta1 = getAssociatedTokenAddressSync(tokenMintA.publicKey, user.publicKey)
+    const [bridge_token_pda] = PublicKey.findProgramAddressSync([Buffer.from("token"), tokenMintA.publicKey.toBuffer()], program.programId)
+    const bridge_token_account = getAssociatedTokenAddressSync(tokenMintA.publicKey, bridge_token_pda, true);
+
+    console.log("userAta1", userAta1.toBase58())
+    console.log("bridge_token_pda", bridge_token_pda.toBase58())
+    console.log("bridge_token_account", bridge_token_account.toBase58())
+
+
+    const tokenBalance = await connection.getTokenAccountBalance(userAta1);
+    console.log("tokenBalance", tokenBalance)
+    const tokenBalanceB = await connection.getTokenAccountBalance(bridge_token_account);
+    console.log("tokenBalanceB", tokenBalanceB)
+
+
+
+    // Lock tokens
+    await program.methods.lock(amount, "0x228").accounts({
+      // authority: admin.publicKey,
+      state: state,
+      sender: user.publicKey,
+      sender_token_account: userAta1,
+      bridge_token: bridge_token_pda,
+      bridgeTokenAccount: bridge_token_account,
+      mint: tokenMintA.publicKey,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      system_program: SystemProgram.programId,
+    }).signers([admin]).rpc();
+
+    const accountState = await program.account.globalState.fetch(state);
+    assert.ok(+accountState.nonce === 1);
+
+    // const nonceAccount = PublicKey.findProgramAddressSync([user.publicKey.toBuffer()], program.programId)[0];
+    // const receiverNonce = await program.account.nonceAccount.fetch(nonceAccount);
+    // console.log(receiverNonce);
+
+
+    const tokenBalance2 = await connection.getTokenAccountBalance(userAta1);
+    console.log(tokenBalance2)
+
+  });
+
+  // it('should unlock tokens with valid nonce and signatures', async () => {
+  //     const amount = new anchor.BN(50);
+  //
+  //     // Mint tokens to the fromTokenAccount
+  //     await tokenMint.mintTo(fromTokenAccount, admin, [], amount.toNumber());
+  //
+  //     // Lock tokens first
+  //     await program.rpc.lock(amount, "destination_string", {
+  //         accounts: {
+  //             authority: admin.publicKey,
+  //             from: fromTokenAccount,
+  //             to: toTokenAccount,
+  //             state,
+  //             tokenProgram: TOKEN_PROGRAM_ID,
+  //         },
+  //         signers: [admin],
+  //     });
+  //
+  //     // Unlock tokens
+  //     await program.rpc.unlock(amount, new anchor.BN(0), {
+  //         accounts: {
+  //             authority: admin.publicKey,
+  //             user: user.publicKey,
+  //             from: fromTokenAccount,
+  //             to: toTokenAccount,
+  //             nonceAccount,
+  //             state,
+  //             tokenProgram: TOKEN_PROGRAM_ID,
+  //             systemProgram: SystemProgram.programId,
+  //         },
+  //         signers: [user],
+  //     });
+  //
+  //     const accountState = await program.account.globalState.fetch(state);
+  //     assert.ok(accountState.nonce === 1);
+  //
+  //     const fromBalance = await tokenMint.getAccountInfo(fromTokenAccount);
+  //     const toBalance = await tokenMint.getAccountInfo(toTokenAccount);
+  //
+  //     assert.ok(fromBalance.amount.toNumber() === amount.toNumber()); // Should have received the tokens back
+  //     assert.ok(toBalance.amount.toNumber() === 0); // Should have transferred out the tokens
+  // });
+
+  // it('should enable and disable the state correctly', async () => {
+  //     // Disable state
+  //     await program.rpc.disable({
+  //         accounts: {
+  //             state,
+  //             authority: admin.publicKey,
+  //         },
+  //         signers: [admin],
+  //     });
+  //
+  //     let accountState = await program.account.globalState.fetch(state);
+  //     assert.ok(!accountState.isEnable);
+  //
+  //     // Enable state
+  //     await program.rpc.enable({
+  //         accounts: {
+  //             state,
+  //             authority: admin.publicKey,
+  //         },
+  //         signers: [admin],
+  //     });
+  //
+  //     accountState = await program.account.globalState.fetch(state);
+  //     assert.ok(accountState.isEnable);
+  // });
+  //
+});
+
+
+export const mintingTokens = async (
+  {
+    connection,
+    creator,
+    holder = creator,
+    tokenKeypair,
+    mintedAmount = 100,
+    decimals = 6,
+  }: {
+    connection: Connection;
+    creator: Signer;
+    holder?: Signer;
+    tokenKeypair: Keypair;
+    mintedAmount?: number;
+    decimals?: number;
+  }) => {
+
+  await createMint(connection, creator, creator.publicKey, creator.publicKey, decimals, tokenKeypair);
+  const ata = await getOrCreateAssociatedTokenAccount(connection, creator, tokenKeypair.publicKey, holder.publicKey, true);
+  await mintTo(connection, creator, tokenKeypair.publicKey, ata.address, creator.publicKey, mintedAmount * 10 ** decimals,);
+  return ata;
+};
+
+
+function hexToUint8Array(hexString: string) {
+  if (hexString.startsWith("0x"))
+    hexString = hexString.slice(2); // Remove "0x" prefix if present
+  const bytes = new Uint8Array(hexString.length / 2);
+  for (let i = 0; i < bytes.length; i++)
+    bytes[i] = parseInt(hexString.substr(i * 2, 2), 16);
+  return bytes;
+}
+
+
+async function requestSol(account, connection, amount=5) {
+  console.log(`Requesting airdrop for SOL : ${account.publicKey.toBase58()}`)
+  const signature = await connection.requestAirdrop(account.publicKey, amount * 10 ** 9);
+  const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+  await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature }, 'finalized');
+  console.log("account wallet balance : ", account.publicKey.toBase58(), (await connection.getBalance(account.publicKey)) / 10 ** 9, "SOL")
+}

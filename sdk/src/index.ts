950300ac233c62f4ebb69e7a518ad234d4b51213
Illia Likhoshva <ilyaliko64@gmail.com>
2025-03-14 22:27:51 +0200

    Add publish sdk workflow

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,10 +1,9 @@
-import * as backend from "./backend";
-import * as evm from "./evm";
-import * as solana from "./solana";
+export * as backend from "./backend";
+export * as evm from "./evm";
+// export * as solana from "./solana";
 export {
   SOLANA_CHAIN_ID,
   SOLANA_DEV_CHAIN_ID,
   rpcs,
   backendUrl,
 } from "./config";
-export { evm, solana, backend };

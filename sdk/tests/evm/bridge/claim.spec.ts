765049e7f0418fc6eb4e6ac71d529b4b0470c06c
Illia Likhoshva <ilyaliko64@gmail.com>
2025-03-18 10:50:22 +0200

    Update test signature for claim receipt

--- sdk/tests/evm/bridge/claim.spec.ts
+++ sdk/tests/evm/bridge/claim.spec.ts
@@ -79,22 +79,6 @@ describe("Test bridge claim request", () => {
   });
   const mockedBridgeAddress: Address =
     "0x5Bcb9233DfEbcec502C1aCce6fc94FefF8c037C3";
-  const mockedRecipient: Hex = toHex("Bob", { size: 32 });
-  const mockedSignature: Hex =
-    "0x4c5908107ce8fe51ac8c09188bb817c070ffed724b52bc8b29baf9905b76efe314392dbf9ace7e23aa3be2113ffb97fe9af438c4ff393d5a0de37b1ba2a28e211b";
-  const mockedPayload: evm.SendPayloadEVM = {
-    amountToSend: 1000000000000000000n,
-    chainFrom: 22040n,
-    chainTo: 6003100671677645902n,
-    externalTokenAddress:
-      "0x0cf53911c806ccffeb6a2cf754a5ec25b87f7a733cf375a90ce5faac9fcf4c5b",
-    feeAmount: 585666821314334878500n,
-    flagData: "0x",
-    flags: 0n,
-    timestamp: 1741897441,
-    tokenAddress:
-      "0x0000000000000000000000002Cf845b49e1c4E5D657fbBF36E97B7B5B7B7b74b",
-  };
   test("Should call claim request successfully", async () => {
     await testClient.impersonateAccount({
       address: AccountFixture.address,

84eab537612cb30a897f6f7bafa27386ddae79b1
svin <svinerus@gmail.com>
2025-04-08 10:08:08 +0300

    remove ambrosus copyright

--- relay/src/solana/utils.ts
+++ relay/src/solana/utils.ts
@@ -1,12 +1,3 @@
-/*
- *  Copyright: Ambrosus Inc.
- *  Email: tech@ambrosus.io
- *
- *  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- *  This Source Code Form is “Incompatible With Secondary Licenses”, as defined by the Mozilla Public License, v. 2.0.
- */
-
 import { Base58, Hex } from "ox";
 import { Buffer } from "buffer";
 import * as borsh from "borsh";

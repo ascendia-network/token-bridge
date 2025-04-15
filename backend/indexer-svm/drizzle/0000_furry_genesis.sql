d3d9cafba6634a2facf05d23997e7155fb4e0af7
Illia Likhoshva <ilyaliko64@gmail.com>
2025-04-15 11:47:40 +0300

    Update SVM schema

--- backend/indexer-svm/drizzle/0000_furry_genesis.sql
+++ backend/indexer-svm/drizzle/0000_furry_genesis.sql
@@ -1,3 +1,5 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
 CREATE SCHEMA "indexer_solana";
 --> statement-breakpoint
 CREATE TABLE "indexer_solana"."receiptsClaimed" (
@@ -7,16 +9,16 @@ CREATE TABLE "indexer_solana"."receiptsClaimed" (
 	"to" text NOT NULL,
 	"token_address_to" text NOT NULL,
 	"amount_to" numeric(78, 0) NOT NULL,
-	"chain_to" numeric(78, 0) NOT NULL,
 	"chain_from" numeric(78, 0) NOT NULL,
+	"chain_to" numeric(78, 0) NOT NULL,
 	"event_id" numeric(78, 0) NOT NULL,
 	"flags" numeric(78, 0) NOT NULL,
-	"data" text NOT NULL,
-	CONSTRAINT "receiptsClaimed_chain_to_event_id_pk" PRIMARY KEY("chain_to","event_id")
+	CONSTRAINT "receiptsClaimed_chain_from_chain_to_event_id_pk" PRIMARY KEY("chain_from","chain_to","event_id")
 );
 --> statement-breakpoint
 CREATE TABLE "indexer_solana"."receiptsMeta" (
 	"receipt_id" text PRIMARY KEY NOT NULL,
+	"event_chain" numeric(78, 0),
 	"block_hash" text,
 	"block_number" numeric(78, 0),
 	"timestamp" numeric(78, 0),
